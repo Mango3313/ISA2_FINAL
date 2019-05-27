8ae8efe5d9cd1ac524e24d92408906f456bfbeff
Mango3313 <daft_noob3313@live.com>
2019-05-27 11:25:33 -0500

    Updates

--- JS/index.js
+++ JS/index.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
         if($(this).scrollTop() < 620){
             $('nav').removeClass().addClass('navbar fixed-top navbar-horizontal navbar-expand-lg navbar-dark bg-info');
         }else
-        if($(this).scrollTop() > 621 && $(this).scrollTop() < 3500){
+        if($(this).scrollTop() > 621 && $(this).scrollTop() < 3000){
             $('nav').removeClass().addClass('navbar fixed-top navbar-horizontal navbar-expand-lg navbar-dark bg-warning');
         }else
-        if($(this).scrollTop() > 3500){
+        if($(this).scrollTop() > 3000){
             $('nav').removeClass().addClass('navbar fixed-top navbar-horizontal navbar-expand-lg navbar-dark bg-success');
         }
     });
@@ -27,6 +27,7 @@ $(document).ready(function(){
             borderColor: "rgba(117,110,255,0.8)",
             backgroundColor:"rgba(117,110,255,0.2)",
             //PPerfil,Presupuesto,Capacitación
+            //1,3,4
             data: [65, 75, 70],
             //fill: false
         },{
@@ -45,6 +46,7 @@ $(document).ready(function(){
             borderColor: "rgba(117,110,255,0.8)",
             backgroundColor:"rgba(117,110,255,0.2)",
             //PPerfil,Presupuesto
+            //2,7
             data: [2, 1],
             //fill: false
         },{
@@ -56,6 +58,7 @@ $(document).ready(function(){
         }]
 
     }
+    //Restantes: 5,6,8,9,10,11,12,13
     var myRadarChart = new Chart(grafica, {
         type: 'radar',
         data: data
@@ -80,6 +83,30 @@ $(document).ready(function(){
     });
 });
 function getResults(){
-    var res = $('#formAns').serialize();
-    console.log(res);
+    //if(validateForm()){
+        var r1 = $("input[name=custom-radio-1]").val();
+        var r2 =$("input[name=custom-radio-2]").val();
+        var r3 =$("input[name=custom-radio-3]").val();
+        var r4 =$("input[name=custom-radio-4]").val();
+        var r5 =$("input[name=custom-radio-5]").val();
+        var r6 =$("input[name=custom-radio-6]").val();
+        var r7 =$("input[name=custom-radio-7]").val();
+        var r8 =$("input[name=custom-radio-8]").val();
+        var r9 =$("input[name=custom-radio-9]").val();
+        var r10 =$("input[name=custom-radio-10]").val();
+        var r11 =$("input[name=custom-radio-11]").val();
+        var r12 =$("input[name=custom-radio-12]").val();
+        var r13 =$("input[name=custom-radio-13]").val();
+        var r14 =$("input[name=custom-radio-14]").val();
+        var r15 =$("input[name=custom-radio-15]").val();
+    if(r1 == '' || r2 == '' || r3 == '' || r4 == '' ||r5 == '' ||
+    r6 == '' ||r7 == '' ||r8 == '' ||r9 == '' ||r10 == '' ||
+    r11 == '' ||r12 == '' ||r13 == '' ||r14 == '' ||r15 == '' ){
+        alert("Uno o mas campos vacios");
+    }
+    //var res = $('#formAns').serialize();
+    //console.log(res);
+    //}else{
+        //console.log("Failed");
+    //}
 }
